8b294ad08aff59b52ac49b5de77e855160f93266
suhani singhal <144513906+suhani821@users.noreply.github.com>
2026-07-09 18:46:46 +0000

    first encounter project to increase or decrease a value by clicking inc or dec buttons

---
+++ encounterproject/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+let [counter,setCounter]=useState(7);
+function addval(){
+  counter = counter+1;
+  setCounter(counter);
+}
+function decval(){
+  setCounter(counter--);
+}
+return(<>
+ <h1>hello world!</h1>
+ <p> counter value {counter}</p>
+ <button onClick={addval}>increase value {counter}</button>
+ <br />
+ <button onClick={decval}>decrease value{counter}</button>
+  </>
+ );
+    
+  
+}
+
+export default App
