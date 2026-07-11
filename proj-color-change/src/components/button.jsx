fa57fe606fc046b08f53a0051fb27b5f7307ea46
suhani singhal <144513906+suhani821@users.noreply.github.com>
2026-07-11 06:03:47 +0000

    button added

---
+++ proj-color-change/src/components/button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Btn({txt="black"}){
+    return(<>
+    <button className='p-0 , mx-440'>{txt}</button>
+    </>);
+
+}
+export default Btn;
--- proj-color-change/src/main.jsx
+++ proj-color-change/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Btn from './components/button.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    
+    <Btn txt="blue"/>
+    <Btn txt="yellow"/>
+    <Btn txt="red"/>
+<Btn    txt="green"/>
+<Btn txt="purple"/>
+    <Btn/>
+    
+  </StrictMode>
 )
